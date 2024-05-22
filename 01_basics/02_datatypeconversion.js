2286a0ea8603be5cdde77bd9d4c3aac2169d6bb0
MahimaBabani14 <babanimahima1@gmail.com>
2024-05-22 14:55:17 +0000

    operations

--- 01_basics/02_datatypeconversion.js
+++ 01_basics/02_datatypeconversion.js
@@ -1,25 +1,61 @@
-let score = true
+// let score = true
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
 
-//"33" => 33
-//'33abc => NaN
-// true => 1; false => 0
+// //"33" => 33
+// //'33abc => NaN
+// // true => 1; false => 0
 
 
-let isLoggedIn = "mahi"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = "mahi"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-// 1 => true; 0 => false
-// "" => false
-// "name" => true
+// // 1 => true; 0 => false
+// // "" => false
+// // "name" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+//  operations  // 
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// let str1 = "mahima"
+// let str2 = " mahi"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);  // 12
+// console.log(1 +2 + "2"); // 32
+
+// console.log("1" + 2 + 3);  // 123
+
+
+console.log(+true); //1
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2
+console.log(num2);
+
+let gameCounter = 100
+gameCounter++;  //postfix
+console.log(gameCounter);
