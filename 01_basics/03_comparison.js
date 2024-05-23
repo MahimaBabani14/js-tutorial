fd449e1874b155bc1e3da49388f2962e1750a649
MahimaBabani14 <babanimahima1@gmail.com>
2024-05-23 07:52:05 +0000

    comparison

---
+++ 01_basics/03_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null== 0);
+// console.log(null >= 0);
+
+
+// console.log("2" == 2);
+// console.log("2" === 2);  // === : it check the datatype ("string")
+
+// we have to avoid these type of cnversions
